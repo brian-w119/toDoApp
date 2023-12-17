8d3bf1ed92bff1a6b582ab212be2a0b142f4d590
MyPc <brianw119@outlook.com>
2023-12-17 23:27:11 +0000

    basic Structure of app created

---
+++ defaultPage.js
@@ -0,0 +1,33 @@
+
+
+//window.grid1 = document.querySelector("grid1");
+//grid1.style.backgroundColor = "pink";
+
+export default function defaultState(){
+
+    const newTask = document.createElement("button");
+    newTask.id = "createNewToDo";
+    newTask.style.width = "120px";
+    newTask.style.height = "40px";
+    newTask.style.backgroundColor = "blue";
+    newTask.style.color = "white";
+    newTask.textContent = "+ New Task";
+    newTask.style.marginTop = "10px";
+    const grid1 = document.querySelector(".grid1");
+    grid1.appendChild(newTask);
+
+    const grid2 = document.querySelector(".grid2");
+    const outstanding = document.createElement("h2");
+    outstanding.textContent = "OUTSTANDING";
+    outstanding.style.display = "flex";
+    outstanding.style.justifyContent = "center";
+    outstanding.style.color = "white";
+    grid2.appendChild(outstanding);
+};
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,4 @@
+
+import defaultState from './defaultPage.js';
+
+defaultState();
