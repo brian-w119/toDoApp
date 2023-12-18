3559ac4042689a459bd2526003028d36ddf7abef
MyPc <brianw119@outlook.com>
2023-12-18 23:01:46 +0000

    Fields for outsanding tasks added

---
+++ outstanding.js
@@ -0,0 +1,43 @@
+
+const grid2 = document.querySelector(".grid2");
+grid2.style.width = "50vw";
+grid2.style.display = "flex";
+grid2.style.justifyContent = "center";
+grid2.style.alignItems = "flex-end";
+grid2.style.position = "relative";
+const gridContainer = document.createElement("div");
+gridContainer.style.display = "flex";
+gridContainer.style.width = "70%";
+gridContainer.style.justifyContent = "space-around";
+gridContainer.style.gap = "20px";
+
+
+const column1 = createColumns();
+const column2 = createColumns();
+const column3 = createColumns();
+
+
+export default function displayOld(){
+
+    column1.style.height = "60vh";
+    column1.style.width = "20vw";
+    column1.style.backgroundColor = "white";
+    gridContainer.appendChild(column1);
+
+    column2.style.height = "60vh";
+    column2.style.width = "20vw";
+    column2.style.backgroundColor = "white";
+    gridContainer.appendChild(column2);
+
+    column3.style.height = "60vh";
+    column3.style.width = "20vw";
+    column3.style.backgroundColor = "white";
+    gridContainer.appendChild(column3);
+
+};
+
+grid2.appendChild(gridContainer);
+
+function createColumns(){
+   return  document.createElement("div");
+};
--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 import defaultState from './defaultPage.js';
 import newTask from './newTask.js';
 import clearData from './clearFields.js';
+import displayOld from './outstanding.js';
 
 defaultState();
 newTask();
 clearData();
+displayOld();
 
 
 
