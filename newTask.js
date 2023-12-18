4cba1792fab9d012d143c86d25b970f25a10e2ae
MyPc <brianw119@outlook.com>
2023-12-18 16:23:42 +0000

    Additional fields added

--- newTask.js
+++ newTask.js
@@ -10,25 +10,45 @@ export default function newTask(){
         
         const task = createField();
         task.type = "text";
-        task.name = "text";
-        task.placeholder = "Title";
-        task.style.display = "block";
-        task.style.marginTop = "10px";
+        task.name = "task";
         task.size = "30";
+        task.placeholder   = "Title";
+        task.style.display = "block";
+        task.style.marginTop = "10vw";
+        task.style.textAlign = "center";
         grid1.appendChild(task);
 
         const details = createField();
         details.type  = "text";
-        details.name = "text";
-        details.placeholder = "Enter Details";
+        details.name  = "details";
+        details.placeholder   = "Enter Details";
         details.style.display = "block";
         details.style.marginTop = "30px";
-        details.style.width = "500px";
         details.style.height = "50px";
-        details.maxLength = "100";
+        details.style.maxWidth  = "500px";
+        details.style.width  = "300px";
+        details.style.height = "50px";
+        details.maxLength    = "100";
+        details.style.textAlign = "center";
         details.style.wordBreak = "break-word";
         grid1.appendChild(details);
 
+        const dueDate = createField();
+        dueDate.type  = "date";
+        dueDate.name  = "date";
+        dueDate.width = "50px";
+        dueDate.style.marginTop = "30px";
+        grid1.appendChild(dueDate);
+
+        const priority = createField();
+        priority.type  = "text";
+        priority.name  = "priority";
+        priority.width = "50px";
+        priority.style.display = "block";
+        priority.style.textAlign = "center";
+        priority.style.marginTop = "30px";
+        priority.placeholder = "Low / Medium / High";
+        grid1.appendChild(priority);
     });
 };
 
